d7db48aa42c75878ce3edd16952f88e63bb368bc
Krishna Shukla <krish@techlify.com>
2020-09-18 12:04:02 +0530

    Updates

---
+++ precache-manifest.5c67ce51130be7f3ebff45dad15f5376.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "908da0788e057775e2652651f65ea70c",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "bdea3745ba12b3cca1bf",
+    "url": "/keeper-app/static/js/2.8122f025.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/keeper-app/static/js/2.8122f025.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4b294c5c32790aac054",
+    "url": "/keeper-app/static/js/main.2b07d13e.chunk.js"
+  },
+  {
+    "revision": "44d6fa134b32989acb5c",
+    "url": "/keeper-app/static/js/runtime-main.3313fec8.js"
+  }
+]);
